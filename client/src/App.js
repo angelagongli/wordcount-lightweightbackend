713a2b87fe54f8ffe1e61395db2cd00103d264c0
angela <angelagongli@gmail.com>
2020-09-25 21:33:38 -0500

    add /src directory

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Count from "./components/Count";
+import API from "./utils/API";
+import 'office-ui-fabric-react/dist/css/fabric.css';
+import "./assets/css/style.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      file: {},
+      isParsing: false,
+      paper: {}
+    };
+    this.updateUpload = this.updateUpload.bind(this);
+  }
+
+  componentDidMount() {  
+    API.getPaper().then(res => {
+      if (res.data) {
+        this.setState({ paper: res.data });
+        API.deletePaper().then(res => {
+          console.log("paper completely processed at App.js");
+        });
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  updateUpload(file) {
+    this.setState({
+      file: file,
+      isParsing: true,
+      paper: {}
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <div className="row no-gutters">
+            <div className="col-12 col-lg-4 align-self-center">
+              <Form updateUpload={this.updateUpload} />
+            </div>
+            <div className="col-12 col-lg-8">
+              <Count
+                fileName={this.state.file.name || this.state.paper.fileName}
+                isParsing={this.state.isParsing}
+                parsedPDF={this.state.paper.parsedPDF}
+                wordCount={this.state.paper.wordCount}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
